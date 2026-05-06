d9ac571310d64d9a51242d2bd27a78c5577ae20f
Anton <antonms2002@gmail.com>
2026-05-07 01:13:25 +0300

    created conftest and first test module

---
+++ test_items.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class TestItems:
+
+    _ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
+
+
+    def test_button_exists(self, browser, wait):
+        browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+
+        button = wait.until(EC.visibility_of_element_located(self._ADD_TO_CART_BUTTON))
+
+        assert button.is_displayed()
