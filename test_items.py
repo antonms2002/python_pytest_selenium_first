8704ec552cd885701aae886e245bae095195d601
Anton <antonms2002@gmail.com>
2026-05-10 14:26:56 +0300

    edit test name: test_button_add_to_cart_exists

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ class TestItems:
     _ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
 
 
-    def test_button_exists(self, browser, wait):
+    def test_button_add_to_cart_exists(self, browser, wait):
         browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
 
         button = wait.until(EC.visibility_of_element_located(self._ADD_TO_CART_BUTTON))
